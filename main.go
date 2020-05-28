09eb99dcc467479d8aa02028d6aaa3cd025fd255
Peter Teich <peter.teich@motor.de>
2020-05-28 16:11:06 +0200

    changes in lesson 2

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/pteich/gosea/status"
 )
 
 func main() {
+	var err error
+
+	logfile, err := os.Create("messages.log")
+	if err != nil {
+		log.Fatalf("error opening log file: %s", err.Error())
+	}
+	defer func() {
+		log.Print("closing log file")
+		logfile.Close()
+	}()
+
+	logger := log.New(os.Stdout, "gosea ", log.LstdFlags)
+
+	sigChan := make(chan os.Signal)
+	defer close(sigChan)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", status.Health)
 
@@ -15,5 +37,18 @@ func main() {
 		Handler: mux,
 	}
 
-	srv.ListenAndServe()
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatalf("error starting server: %s", err.Error())
+		}
+	}()
+
+	logger.Print("starting service")
+
+	<-sigChan
+
+	srv.Close()
+
+	logger.Print("stopping service")
 }
